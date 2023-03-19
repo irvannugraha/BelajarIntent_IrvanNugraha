bd420ac6581146a19b853ab4da52244b013065e0
unknown <irvannugraha>
2023-03-19 18:44:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajar_intent_ipan/PindahActivity.kt
@@ -0,0 +1,11 @@
+package com.example.belajar_intent_ipan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PindahActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pindah)
+    }
+}
